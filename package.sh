14fd3f76cfb783a12431bb6eec3284de0334b3a8
Sasha Aliashkevich <olsender@gmail.com>
2015-05-22 10:31:04 +0200

    styles fixes

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -rf ./PGTabs.app
+rm ./pgtabs.dmg
 electron-packager ./ PGTabs --platform=darwin --arch=x64 --version=0.25.2 --icon logo.icns
 hdiutil create -format UDZO -srcfolder PGTabs.app pgtabs.dmg
