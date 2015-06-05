5e0b5f3f1d1fbcfabc345e796c1deafd726c87d6
Sasha Aliashkevich <olsender@gmail.com>
2015-06-05 18:58:18 +0200

    linux build script

---
+++ linux_build.sh
@@ -0,0 +1,48 @@
+rm -rf .build
+mkdir .build
+jsx src/ build/
+cp index.html .build/
+cp main.js .build/
+cp logo.png .build/
+cp package.json .build/
+cp -r build .build/
+cp -r css .build/
+cp -r driver .build/
+
+# bootstrap
+mkdir -p .build/node_modules/bootstrap
+cp -r node_modules/bootstrap/dist .build/node_modules/bootstrap/
+# brace
+cp -r node_modules/brace .build/node_modules/
+
+# c3
+cp -r node_modules/c3 .build/node_modules/
+
+# flux
+cp -r node_modules/flux .build/node_modules/
+# jquery
+cp -r node_modules/jquery .build/node_modules/
+
+# libpq
+cp -r node_modules/libpq .build/node_modules/
+
+# lowdb
+cp -r node_modules/lowdb .build/node_modules/
+
+# microevent
+cp -r node_modules/microevent .build/node_modules/
+
+# react
+cp -r node_modules/react .build/node_modules/
+
+# react-bootstrap
+cp -r node_modules/react-bootstrap .build/node_modules/
+
+# jsdom
+cp -r node_modules/jsdom .build/node_modules/
+
+
+cd .build
+electron-packager ./ "sqltabs" --platform=linux --arch=x64 --version=0.27.1 --icon logo.png --asar --prune
+cd ..
+
