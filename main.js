73d73dd10d690f2cf645d9eb5d5f455fc8c4deb9
Sasha Aliashkevich <olsender@gmail.com>
2017-02-07 13:11:14 +0100

    Open SQLDocument implementation

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 if (process.platform == 'linux'){
     mkdirp = require('mkdirp');
     mkdirp(process.env.HOME+'/.local/share/icons', function(err){console.log(err)});
@@ -26,23 +27,40 @@ var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 
 var mainWindow = null;
+var file2open = null;
+
+var createWindow = function(){
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'SQL Tabs',
+    });
+    mainWindow.maximize();
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
 
 app.on('window-all-closed', function() {
     app.quit();
 });
 
+app.on('open-file', function(event, path){
+    event.preventDefault();
+    file2open = path;
+    if (mainWindow != null){
+        var contents = mainWindow.webContents;
+        contents.send('open-file', file2open);
+    }
+});
+
 app.on('ready', function() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    title: 'SQL Tabs',
-  });
-  mainWindow.maximize();
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
-
+    createWindow();
+    if (file2open != null){
+        var contents = mainWindow.webContents;
+        contents.on('did-finish-load', function(){
+            contents.send('open-file', file2open);
+        });
+    }
 });
--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 /*
   Copyright (C) 2015  Aliaksandr Aliashkevich
-  
+
       This program is free software: you can redistribute it and/or modify
       it under the terms of the GNU General Public License as published by
       the Free Software Foundation, either version 3 of the License, or
       (at your option) any later version.
-  
+
       This program is distributed in the hope that it will be useful,
       but WITHOUT ANY WARRANTY; without even the implied warranty of
       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
       GNU General Public License for more details.
-  
+
       You should have received a copy of the GNU General Public License
       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -25,6 +25,11 @@ var Config = require('./Config');
 var Actions = require('./Actions');
 var CloudMessage = require('./CloudMessage');
 var UpgradeMessage = require('./UpgradeMessage');
+
+require('electron').ipcRenderer.on('open-file', function(event, path) {
+    Actions.newTab();
+    Actions.openFile(path);
+})
 
 require('./Menu');
 
